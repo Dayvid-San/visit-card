1feadba7ae3d3d56a765710e47c24f68c5d1df5b
Dayvid Santana da Silva <77745454+Dayvid-San@users.noreply.github.com>
2026-01-12 19:43:48 -0400

    Create VoiceProvider.tsx

---
+++ components/VoiceProvider.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { useVoiceAnnouncement } from '@/hooks/useVoiceAnnouncement';
+import type React from 'react';
+
+export function VoiceProvider({ children }: { children: React.ReactNode }) {
+  useVoiceAnnouncement();
+  return <>{children}</>;
+}
